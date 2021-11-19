da2b3960e0a6fc9f7c7642407a3076ce1716d3ff
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-19 18:07:59 -0500

    backend-server setup

--- homebidder-api/routes/users.js
+++ homebidder-api/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = ({
 
-module.exports = router;
+}) => {
+  /* GET users listing. */
+  router.get('/', (req, res) => {
+    res.send('respond with a resource');
+  });
+  return router;
+}
