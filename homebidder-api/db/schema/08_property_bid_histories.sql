bc13a44ad50c01e94748d4a72e7e1395b94f0e50
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-19 21:01:48 -0500

    property_bid_hhistory table

---
+++ homebidder-api/db/schema/08_property_bid_histories.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS property_bid_histories CASCADE;
+
+CREATE TABLE property_bid_histories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  bid_amount INTEGER  NOT NULL,
+  seller_response VARCHAR(255) NOT NULL DEFAULT 'Pending',
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
