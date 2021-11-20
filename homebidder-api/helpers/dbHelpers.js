37e123449dc2bc5089ac3fadfe1322fcf2587f5a
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-20 12:44:47 -0500

    Users list and user registration db queries

--- homebidder-api/helpers/dbHelpers.js
+++ homebidder-api/helpers/dbHelpers.js
@@ -1,6 +1,46 @@
 module.exports = (db) => {
+  //GET ALL USERS
+  const getUsers = () => {
+    const query = {
+        text: 'SELECT * FROM users',
+    };
+
+    return db
+        .query(query)
+        .then((result) => result.rows)
+        .catch((err) => err);
+  };
+  //CHECK IF USER EMAIL ALREADY EXIST
+  const getUserByEmail = email => {
+
+    const query = {
+        text: `SELECT * FROM users WHERE email = $1` ,
+        values: [email]
+    }
+
+    return db
+        .query(query)
+        .then(result => result.rows[0])
+        .catch((err) => err);
+  }
+
+  //ADD USER
+  const addUser = (firstName, lastName, email, password) => {
+    const query = {
+        text: `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *` ,
+        values: [firstName, lastName, email, password]
+    }
+
+    return db.query(query)
+        .then(result => result.rows[0])
+        .catch(err => err);
+  }
+
+
 
   return {
-
+    getUsers,
+    addUser,
+    getUserByEmail
   };
 };
