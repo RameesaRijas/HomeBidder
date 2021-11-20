8babd858e2631f961dab09ff7ee3a081c43819ba
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-19 20:41:29 -0500

    bids table schema

--- homebidder-api/db/schema/02_properties.sql
+++ homebidder-api/db/schema/02_properties.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS properties CASCADE;
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  title VARCHAR(255) NOT NULL,
-  description TEXT,
 
   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
