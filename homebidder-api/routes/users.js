0e6c59638f545a70770c911655afffd73b4b8c29
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-20 12:44:17 -0500

    User login, register routes

--- homebidder-api/routes/users.js
+++ homebidder-api/routes/users.js
@@ -1,13 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
 
-/* GET users listing. */
 module.exports = ({
+  getUsers,
+  getUserByEmail,
+  addUser
 
 }) => {
   /* GET users listing. */
   router.get('/', (req, res) => {
-    res.send('respond with a resource');
+    getUsers()
+    .then((users) => res.json(users))
+    .catch((err) => res.json({
+        error: err.message
+    }));
   });
+//register
+  router.post('/register', (req,res) => {
+    const {
+      first_name,
+      last_name,
+      email,
+      password
+    } = req.body;
+
+    getUserByEmail(email)
+      .then(user => {
+          if (user) {
+              res.json({
+                  msg: 'Sorry, a user account with this email already exists'
+              });
+          } else {
+              const passwordHashed = bcrypt.hashSync(password, salt);
+              return addUser(first_name, last_name, email, passwordHashed)
+          }
+
+      })
+      .then(newUser => res.json(newUser))
+      .catch(err => res.json({
+        error: err.message
+      }));
+  })
+
+  //login to check and compare password
+  router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    getUserByEmail(email)
+      .then(user => {
+        if (user) {
+          if (bcrypt.compareSync(password, user.password)) {
+            return user;
+          } else {
+            res.json({
+              msg: 'Wrong Credentials'
+            });
+          }
+        }
+      })
+      .then(user => res.json(user))
+      .catch(err => res.json({
+        error: err.message
+      }));
+  })
+
   return router;
 }
