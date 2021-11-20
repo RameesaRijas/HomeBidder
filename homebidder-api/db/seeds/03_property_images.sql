f3462fd55bc5be7a3b9c309a028997492735d2ae
mays4 <maysalani84@yahoo.com>
2021-11-20 11:34:02 -0500

    seeds properties and proerty_images

---
+++ homebidder-api/db/seeds/03_property_images.sql
@@ -0,0 +1,28 @@
+INSERT INTO
+property_images(property_id,image_url)
+VALUES
+    (2,'./images/1_1.png'), 
+    (2,'./images/1_2.png'),
+    (2,'./images/1_3.png'),
+    (2,'./images/1_4.png'),
+    (2,'./images/1_5.png'),
+    (3,'./images/2_1.png'),
+    (3,'./images/2_2.png'),
+    (3,'./images/2_3.png'),
+    (3,'./images/2_4.png'),
+    (3,'./images/2_5.png'),
+    (4,'./images/3_1.png'),
+    (4,'./images/3_2.png'),
+    (4,'./images/3_3.png'),
+    (4,'./images/3_4.png'),
+    (4,'./images/3_5.png'),
+    (5,'./images/4_1.png'),
+    (5,'./images/4_2.png'),
+    (5,'./images/4_3.png'),
+    (5,'./images/4_4.png'),
+    (5,'./images/4_5.png'),
+    (6,'./images/5_1.png'),
+    (6,'./images/5_2.png'),
+    (6,'./images/5_3.png'),
+    (6,'./images/5_4.png'),
+    (6,'./images/5_5.png');
