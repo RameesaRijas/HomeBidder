c25981f2450f46f506fa04d69a5c869db5a9e04c
HL <HL@HLs-MacBook-Pro.local>
2021-11-21 12:04:42 -0500

    Set up NavBar component

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+
+
+
+export default function NavBar() {
+
+  return (
+    <div classname="navbar">
+      <h1>HomeBidder Navbar</h1>
+    </div>
+
+  )
+}
