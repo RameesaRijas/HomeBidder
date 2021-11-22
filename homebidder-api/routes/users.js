9b2b1fbed49a7213aeb71d2281a9b039a07b9b13
mays4 <maysalani84@yahoo.com>
2021-11-21 22:18:55 -0500

    adding route and jwt and authentaction

--- homebidder-api/routes/users.js
+++ homebidder-api/routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const salt = bcrypt.genSaltSync(10);
+const jwt = require("jsonwebtoken");
+
+
 
 module.exports = ({
   getUsers,
@@ -19,6 +22,7 @@ module.exports = ({
   });
 //register
   router.post('/register', (req,res) => {
+    console.log(req.body)
     const {
       first_name,
       last_name,
@@ -29,11 +33,14 @@ module.exports = ({
     getUserByEmail(email)
       .then(user => {
           if (user) {
+            
               res.json({
                   msg: 'Sorry, a user account with this email already exists'
               });
           } else {
+            
               const passwordHashed = bcrypt.hashSync(password, salt);
+              console.log("pass",passwordHashed)
               return addUser(first_name, last_name, email, passwordHashed)
           }
 
@@ -46,26 +53,50 @@ module.exports = ({
 
   //login to check and compare password
   router.post('/login', (req, res) => {
+    
     const {email, password} = req.body;
+   
     getUserByEmail(email)
       .then(user => {
         if (user) {
+         
           if (bcrypt.compareSync(password, user.password)) {
-            return user;
+          
+            console.log("password match")
+            const token = jwt.sign({
+              email : req.body.email,},"secret123")
+            
+            res.json({auth:true,token:token,user:user})
           } else {
-            res.json({
+            res.json({ auth :false,
+           
               msg: 'Wrong Credentials'
             });
           }
         }
       })
-      .then(user => res.json(user))
       .catch(err => res.json({
         error: err.message
       }));
   })
-
-
-
+  const verfiyjwt = (req,res,next)=> {
+    const token = req.headers['x-access-token']
+    if(!token){
+      res.send("you need a token")
+    } else {
+      jwt.verify(token,"secret123",(err,token)=>{
+        if(err){
+          res.json({auth:false,message:"you are not authenticated"})
+        } else {
+          req.email = token.email
+          next()
+        }
+    })
+    }
+};
+  
+router.get('/userAuth',verfiyjwt,(req,res) => {
+  res.send("you are  authenticated")
+})
   return router;
 }
