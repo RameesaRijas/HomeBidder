8c7af28a9772b6d85057d2f937fd759e3ccadc2f
mays4 <maysalani84@yahoo.com>
2021-11-21 22:13:28 -0500

    login/register connect to backend and login  authentication

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import "./Login.css";
 
+import "./Login.css";
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
 export default function Login() {
+    const [userEmail, setUserEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginStatus, setLoginStatus] = useState(false);
+    const login = (e) => {
+        e.preventDefault()
+        axios.post("api/users/login", {
+          email: userEmail,
+          password: password,
+        }).then((response) => {
+            console.log(response.data)
+          if (!response.data.auth) {
+            setLoginStatus(false);
+          } else {
+              console.log(response.data)
+              localStorage.setItem("token",response.data.token)
+             setLoginStatus(true);
+          }
+        });
+      };
+    
+      useEffect(() => {
+        axios.get("api/users/").then((response) => {
+          if (response.data.loggedIn === true) {
+            setLoginStatus(response.data.email);
+          }
+        });
+      }, []);
+     const userAuth = ()=>{
+         axios.get("api/users/userAuth",{
+            headers:{
+          'x-access-token':localStorage.getItem("token")
+         }}).then((response)=>{
+            console.log(response)
+         })
+     }
+
+     const logout =()=> {
+         setLoginStatus(false)
+         localStorage.setItem("token","")
+     }
+      
   return(
     <div className="login">
-    <form className="loginform">
+        {!loginStatus ? "please login to continue":(<button onClick={logout}>logout</button>)}
+    <form className="loginform" onSubmit={login} >
         <div className="form-group ">
         <label >Email </label>
         <input type="email" 
                className="form-control" 
                id="email" 
                placeholder="Enter email"
+               onChange={(e) => {
+                setUserEmail(e.target.value)}}
         />
         </div>
         <div className="form-group">
@@ -19,17 +64,20 @@ export default function Login() {
                 className="form-control" 
                 id="password" 
                 placeholder="Password"
+                onChange={(e) => {
+                    setPassword(e.target.value)}}
             />
         </div>
        
         <div className="form-group">
-        <button 
+        <button
             type="submit" 
             className="login-button" >
             Submit
-        </button>
+        </button>  
         </div>
     </form>
+    {/* <button onClick={userAuth}>check auth </button> */}
 </div>
 )
 }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,55 +1,77 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import './Register.css';
 export default function RegistForm(props) {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+   const [userEmailReg, setUseEmailReg] = useState("");
+   const [passwordReg, setPasswordReg] = useState("");
+   const register = (e) => {
+    e.preventDefault()
+    axios.post("api/users/register", {
+      first_name:firstName,
+      last_name:lastName,
+      email:userEmailReg,
+      password: passwordReg
+      
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+    
   return(
         <div className="register">
-            <form className="registerform">
-                <div className="form-group ">
-                <label >Email </label>
-                <input type="email" 
-                       className="form-control" 
-                       id="email" 
-                       placeholder="Enter email"
-                />
-                
-                </div>
-                <div className="form-group">
+            <form className="registerform" onSubmit={register}>
+            <div className="form-group">
                     <label >First Name</label>
                     <input type="firstName" 
                         className="form-control" 
                         id="firstName" 
                         placeholder="First Name"
+                        onChange={(e) => {
+                            setFirstName(e.target.value)}}
                     />
                 </div>
                 <div className="form-group">
                     <label>Last Name</label>
                     <input type="LastName" 
                         className="form-control" 
-                        id="firstName" 
+                        id="lastName" 
                         placeholder="Last Name"
+                        onChange={(e) => {
+                            setLastName(e.target.value)}}
                     />
                 </div>
+
+                <div className="form-group ">
+                <label >Email </label>
+                <input type="email" 
+                       className="form-control" 
+                       id="email" 
+                       placeholder="Enter email"
+                        value={userEmailReg}
+                       onChange={(e) => {
+                        setUseEmailReg(e.target.value)}}
+                />
+                
+                </div>
+               
                 <div className="form-group">
                     <label >Password</label>
                     <input type="password" 
                         className="form-control" 
                         id="password" 
                         placeholder="Password"
-                    />
-                </div>
-                <div className="form-group ">
-                    <label >Confirm Password</label>
-                    <input type="password" 
-                        className="form-control" 
-                        id="confirmPassword" 
-                        placeholder="Confirm Password"
+                        onChange={(e) => {
+                        setPasswordReg(e.target.value)}}
+
                     />
                 </div>
                 <div className="form-group">
                 <button 
                     type="submit" 
                     className="regiter-button" >
-                    Register
+                    register
                 </button>
                 </div>
             </form>
