cef68f4eec3245ac1033ae761975988a3e46f3c3
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-23 19:18:49 -0500

    making state array for fav

--- frontend/src/reducer/Property.js
+++ frontend/src/reducer/Property.js
@@ -1,5 +1,6 @@
 export const SET_PROPERTY_DATA = "SET_PROPERTY_DATA";
 export const SET_PROPERTY_ID = "SET_PROPERTY_ID";
+export const SET_FAV = "SET_FAV";
 
 
 function reducer(state, action) {
@@ -8,7 +9,7 @@ function reducer(state, action) {
       return {  
         ...state,
         properties: action.properties,
-        bidders: action.bidders
+        fav: action.fav,
       }
 
       case SET_PROPERTY_ID:
@@ -16,6 +17,19 @@ function reducer(state, action) {
         ...state,
         propertyId: action.id,
       }
+
+      case SET_FAV:
+        let idAlreadyExists = state.fav.some((el)=> el.id === action.id )
+        let favCopy = [...state.fav];
+        if (idAlreadyExists) {
+          favCopy = favCopy.filter(item => item.id !== action.id);
+         } else {
+          favCopy = [...favCopy, action.fav]
+         }
+        return {
+          ...state,
+          fav: favCopy
+        }
 
     default:
       throw new Error(
