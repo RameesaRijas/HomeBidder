d996875208dce5974cf1de33a1c655eadc41259b
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-25 14:51:15 -0500

    used useContext to be used from provider

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,37 +1,49 @@
-
 import "./Login.css";
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect, useContext} from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import {Button} from 'react-bootstrap'
-import { Modal } from 'react-bootstrap'
+import {Button} from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import { propertyContext } from '../providers/PropertyProvider';
+
+
 export default function Login() {
+
+    const { setLoggedInUser } = useContext(propertyContext);
+
     const [userEmail, setUserEmail] = useState("");
     const [password, setPassword] = useState("");
+    //can be removed
     const [loginStatus, setLoginStatus] = useState(false);
-    // const [type,setType]= useState("");
+
     const login = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         axios.post("api/users/login", {
           email: userEmail,
           password: password,
         }).then((response) => {
             console.log(response.data)
           if (!response.data.auth) {
-            setLoginStatus(false);
+            // setLoginStatus(false);
           } else {
-        
+
+              setLoggedInUser(response.data.user);
+
+            //this item can be removed
               localStorage.setItem("user",response.data.user)
               localStorage.setItem("usertype",response.data.user.user_type)
               localStorage.setItem("email",userEmail)
               localStorage.setItem("token",response.data.token)
-               window.location.reload(false);
-             setLoginStatus(true);
+              e.preventDefault();
+
+              //this can be removed //page refresh happening  
+              window.location = "/";
+              setLoginStatus(true);
 
           }
         });
       };
       
+///can be removed ?
       useEffect(() => {
         axios.get("api/users/").then((response) => {
           if (response.data.loggedIn === true) {
@@ -47,78 +59,71 @@ export default function Login() {
             console.log(response)
          })
      }
-
+///delete all codes commented and unwanted
     //  const logout =()=> {
     //      setLoginStatus(false)
          
     //      localStorage.setItem("token","")
         
     //  }
-     const history = useHistory();
+     //const history = useHistory();
       
-      const handleClick = () => history.push("/login");
+      // const handleClick = () => history.push("/login");
       
-      const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
   
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
   return(
     <div className="login">
-        
-         {!loginStatus && "please login to continue"} 
-         <Button variant="primary" onClick={handleShow}>
-              login
-           </Button>
-           <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-        <Modal.Title>log in to continue</Modal.Title>
+      {!loginStatus && "please login to continue"} 
+      < Button variant="primary" onClick={handleShow}>
+          login
+        </Button>
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>log in to continue</Modal.Title>
           </Modal.Header>
-          <Modal.Body>
-       
-    <form className="loginform" onSubmit={login} >
-        <div className="form-group ">
-        <label >Email </label>
-        <input type="email" 
-               className="form-control" 
-               id="email" 
-               placeholder="Enter email"
-               value={userEmail}
-               onChange={(e) => {
-                setUserEmail(e.target.value)}}
-        />
-        </div>
-        <div className="form-group">
-            <label >Password</label>
-            <input type="password" 
+        <Modal.Body>
+          <form className="loginform" onSubmit={login} >
+            <div className="form-group ">
+              <label >Email </label>
+              <input type="email" 
                 className="form-control" 
-                id="password" 
-                placeholder="Password"
-                value={password}
+                id="email" 
+                placeholder="Enter email"
+                value={userEmail}
                 onChange={(e) => {
-                    setPassword(e.target.value)}}
-            />
-        </div>
-       
-        <div className="form-group">
-        <button
-            type="submit" 
-       
-            className="login-button" >
-            Submit
-        </button>  
-        </div>
-    </form>
-    </Modal.Body>
-               <Modal.Footer>
-            <Button className="regiter-button" onClick={handleClose}>
-              cancel
-            </Button>
-           
-           
-          </Modal.Footer>
-    </Modal>
-    
-    {/* <button onClick={userAuth}>check auth </button> */}
-</div>
-)
+                setUserEmail(e.target.value)}}
+              />
+            </div>
+            <div className="form-group">
+              <label >Password</label>
+              <input type="password" 
+                  className="form-control" 
+                  id="password" 
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => {
+                      setPassword(e.target.value)}}
+              />
+            </div>
+            <div className="form-group">
+              <button
+                type="submit" 
+                className="login-button" >
+                Submit
+              </button>  
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="regiter-button" onClick={handleClose}>
+            cancel
+          </Button> 
+        </Modal.Footer>
+      </Modal>
+    </div>
+);
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,27 +1,31 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Navbar, Nav, NavDropdown,ListGroup } from 'react-bootstrap';
-
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { propertyContext } from '../providers/PropertyProvider';
 
 export default function NavBar() {
+
+  const {setLoggedInUser, state} = useContext(propertyContext);
+  const user = state.loggedUser;
+///can be removed
   const isuserLoggedin = localStorage.getItem("token") !== ""
   const useremail = localStorage.getItem("email")
   const type = localStorage.getItem("usertype")
   const userid= localStorage.getItem("userid")
 
   const logout =()=> {
-  
+    setLoggedInUser("");
+    ///can be removed
     localStorage.setItem("token","")
     localStorage.setItem("email","")
     localStorage.setItem("usertype","")
     localStorage.setItem("userid","")
     window.location.reload(false);
 }
-
   return (
-    <Navbar className="homebidder-nav" variant="dark" bg="dark" sticky="top" >
+    <Navbar className="homebidder-nav" variant="dark" bg="dark" sticky="top" collapseOnSelect expand="lg">
         <Navbar.Brand as={Link} to="/">
           <img
           alt="homebidder-icon"
@@ -32,28 +36,19 @@ export default function NavBar() {
           />{' '}
           HomeBidder
         </Navbar.Brand>
-       
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          
         <Nav className="ms-auto">
-       
           <Nav.Link as={Link} to="/">Listings</Nav.Link>
+          {!user.length && (
+            <>
+            <Nav.Link as={Link} to="/login" >Login</Nav.Link>
+            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+            </>
+          )}
         
-            <div>
-              <ul> <li className="nav">
-
-            <Nav.Link href="/login" >Login</Nav.Link>
-            <Nav.Link href="/register">Register</Nav.Link>
-            </li></ul>
-            </div>
-            {isuserLoggedin &&  (
-             <div className="ms-auto">
-             <p className="text-white bg-dark"> welcome{useremail}</p>
-               </div> )}
-        
-               {(isuserLoggedin  && type == 2) && (
-          <NavDropdown title="User" id="navbarScrollingDropdown">
+          {(user  && user.user_type === 2) && (
+          <NavDropdown title={user.email} id="navbarScrollingDropdown">
             <NavDropdown.Item as={Link} to="getRoute">My Favourites</NavDropdown.Item>
             <NavDropdown.Item as={Link} to="getRoute">My Bids</NavDropdown.Item>
             <NavDropdown.Item as={Link} to="getRoute">My Listings</NavDropdown.Item>
@@ -63,11 +58,11 @@ export default function NavBar() {
             <NavDropdown.Item  onClick={logout} as={Link} to="logout">
               Logout
             </NavDropdown.Item> 
-            
           </NavDropdown>
-               )}
-            {(isuserLoggedin  && type == 1) && (
-          <NavDropdown title="Admin" id="navbarScrollingDropdown">
+          )}
+
+          {(user  && user.user_type === 1) && (
+          <NavDropdown title={user.email} id="navbarScrollingDropdown">
             <NavDropdown.Item as={Link} to="getRoute">Pending Listings</NavDropdown.Item>
             <NavDropdown.Item as={Link} to="getRoute">My Bids</NavDropdown.Item>
             <NavDropdown.Divider />
@@ -75,13 +70,9 @@ export default function NavBar() {
               Logout
             </NavDropdown.Item>
           </NavDropdown>
-            )}
-
-           
-          </Nav>
-      
-        </Navbar.Collapse>
-      
+          )}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
