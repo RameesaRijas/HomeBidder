58f253bd485547852351c1e9d03b35709bf3cfef
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-25 14:47:50 -0500

    implemented provider

---
+++ frontend/src/providers/PropertyProvider.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { useReducer, useEffect, createContext } from "react";
+import reducer, {
+  SET_PROPERTY_DATA,
+  SET_PROPERTY_ID,
+  SET_FAV,
+  SET_LOGGED_USER,
+
+} from '../reducer/Property';
+
+export const propertyContext = createContext();
+
+
+
+export default function PropertyProvider(props) {
+  const [state, dispatch] = useReducer(reducer, {
+    propertyId: 0,
+    properties: [],
+    fav:[],
+    loggedUser: {}
+  });
+
+console.log("state", state);
+  const setPropertyId = id => dispatch({ type: SET_PROPERTY_ID, id});
+  const setLoggedInUser = (user) => {
+    if (!user) {
+      axios.post(`/api/users/logout`)
+      .then(res => console.log(res));
+    }
+    dispatch({type : SET_LOGGED_USER, user});
+  }
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/properties'),
+      axios.get('/api/properties/favorites/all'),
+      axios.get('/api/users/getUser')
+    ])
+    .then(
+      ([{ data: properties }, { data: fav }, {data:loggedUser}]) =>
+        dispatch({
+          type: SET_PROPERTY_DATA,
+          properties,
+          fav,
+          loggedUser
+        })
+      )
+    .catch(e => console.log(e));
+  }, []);
+
+
+
+
+  const addToYourFav = (user_id, property_id) => {
+    return axios.post(`/api/properties/favorites/new`, {property_id,user_id})
+      .then(res => {
+        const fav = res.data;
+        const id = fav.id;
+        dispatch({type:SET_FAV, id, fav});
+      })
+  }
+
+  const removeFromFav = (user_id, property_id) => {
+    return axios.delete(`/api/properties/favorites/${property_id}`, {data : {data : user_id}})
+      .then(res => {
+        const fav = res.data; 
+        const id = fav.id;
+        dispatch({type:SET_FAV, id, fav});
+      })
+  }
+
+  const data = {state, setPropertyId, addToYourFav, removeFromFav, setLoggedInUser};
+
+  return (
+    <propertyContext.Provider value={data}>
+      { props.children }
+    </propertyContext.Provider>
+  )
+}
