0ee45ac72bae3be35dd5998614521dc1b3bfdc87
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-28 14:48:26 -0500

    property bid history change table added user_id

--- homebidder-api/db/schema/08_property_bid_histories.sql
+++ homebidder-api/db/schema/08_property_bid_histories.sql
@@ -4,6 +4,7 @@ CREATE TABLE property_bid_histories (
   id SERIAL PRIMARY KEY NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   bid_amount INTEGER  NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   seller_response VARCHAR(255) NOT NULL DEFAULT 'Pending',
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
