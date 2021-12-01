b45be269963e0497880a33cbf10e1f843195c222
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-30 19:53:59 -0500

    added two more users

--- homebidder-api/db/seeds/01_users.sql
+++ homebidder-api/db/seeds/01_users.sql
@@ -7,4 +7,7 @@ INSERT INTO users ( first_name, last_name, email, password) VALUES
   ( 'Robert', 'Robertson', 'robert@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
   ( 'Amy', 'Proctor', 'amy@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
   ( 'Daniel', 'Radcliffe', 'carl@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-  ( 'Emma', 'Watson', 'emma@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+  ( 'Emma', 'Watson', 'emma@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Mays', 'Al', 'mays@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Hasan', 'Lam', 'hasan@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
