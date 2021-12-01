f4d5e38223ac2cee431d8928d81934bfa0c49d62
HL <HL@HLs-MacBook-Pro.local>
2021-11-30 19:58:29 -0500

    Change slogan in Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,9 +3,9 @@ import './Header.css';
 export default function Header() {
   return (
     <div className="header-frame">
-      <img className="tp-bgimg defaultimg" data-bgcolor="undefined" 
+      <img className="tp-bgimg defaultimg" data-bgcolor="undefined"
       src="https://www.godrejsproperty.com/assets/media/property_banner.jpg" alt="heade_img"/>
-      <h2 className="text-frame">Your Dream Home is Here!</h2>
+      <h2 className="text-frame">Bid Goodbye to Blind Bidding!</h2>
     </div>
   );
 }
